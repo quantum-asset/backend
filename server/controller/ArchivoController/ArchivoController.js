1c7977c0cc945f09af79abc95fe5bc7cb0cfebec
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2021-11-08 23:41:51 -0500

    [files]

--- server/controller/ArchivoController/ArchivoController.js
+++ server/controller/ArchivoController/ArchivoController.js
@@ -1,8 +1,74 @@
+import { Hasher } from "../../../utils/utils.js";
 import { connectMysql } from "../../mysql_conector.js";
 import { newResponse } from "../../response/Response.js";
+import { UsuarioController } from "../UsuarioController/UsuarioController.js";
+
+const userController = new UsuarioController();
 export class ActivoController {
   constructor() {
-    store = store;
-    edit = edit;
+    storeFileUser = storeFileUser;
+    storeActivo = storeActivo;
+    //edit = edit;
   }
 }
+const storeFileUser = (idUser, file) => {
+  return new Promise(async (resolve, reject) => {
+    const conn = connectMysql;
+    //add timestamps
+    //console.log("sha", SHA);
+    //console.log("h256", sha256);
+    const newHashedName = Hasher.random();
+    const ruta = process.env.RUTA_ARCHIVOS;
+    const fileToSend = {
+      TIPO_ARCHIVO: file.mimeType,
+      NOMBRE_ARCHIVO: newHashedName,
+      RUTA: ruta,
+      FECHA_CREACION: new Date(),
+      ULTIMA_MODIFICACION: new Date(),
+    };
+    const files = [fileToSend];
+    const query = `INSERT INTO ARCHIVO (TIPO_ARCHIVO, NOMBRE_ARCHIVO, RUTA,FECHA_CREACION,ULTIMA_MODIFICACION) VALUES ?`;
+
+    const values = files.map((x) => [
+      x.TIPO_ARCHIVO,
+      x.NOMBRE_ARCHIVO,
+      x.RUTA,
+      x.FECHA_CREACION,
+      x.ULTIMA_MODIFICACION,
+    ]);
+
+    console.log("archivo:", values);
+    if (conn) {
+      conn.query(query, [values], async (err, result) => {
+        if (err) {
+          console.log("Error al insertar archivo en usuario", err);
+          resolve(Response.error("Error al insertar archivo en usuario"));
+        } else {
+          //EN PAYLOAD ESTA EL insertId
+          const { insertId } = result;
+          //ahora update en user
+          const usuarioRespuesta = await controller.edit(idUser, {
+            ID_ARCHIVO: insertId,
+          });
+          if (usuarioRespuesta.status === "error") {
+            resolve(
+              Response.error("Error al editar usuario ", {
+                ID_ARCHIVO: insertId,
+              })
+            );
+          } else {
+            resolve(
+              Response.ok(
+                "success",
+                { usuarioResult: usuarioRespuesta, archivoResult: result },
+                "Se registró el archivo en usuarios correctamente"
+              )
+            );
+          }
+        }
+      });
+    } else {
+      resolve(Response.error("Error al conectar con la base de datos"));
+    }
+  });
+};
--- server/router/ArchivoRouter.js
+++ server/router/ArchivoRouter.js
@@ -1,29 +1,42 @@
 import express from "express";
 import mysql from "mysql";
-
+import { Response } from "../response/Response";
 
 const ArchivoRouter = express.Router();
 
 //ArchivoRouter = express.Router();
 
 //listar todo
-ArchivoRouter.get("/archivo/:id",(req, res)=>{
+ArchivoRouter.get("/archivo/:id", (req, res) => {});
+
+//insertar 1 o varios
+ArchivoRouter.post("/archivo", (req, res) => {});
+
+//editar uno
+ArchivoRouter.put("/archivo", (req, res) => {});
+
+//editar uno
+ArchivoRouter.patch("/archivo", (req, res) => {});
+
+//eliminar 1
+ArchivoRouter.delete("/archivo/:id", (req, res) => {});
+
+//insertar 1 o varios
+ArchivoRouter.post("/archivo/usuario/:id", (req, res) => {
+  if (!req.files || req.files.file) {
+    res.send(Response.error("No se enviaron archivos"));
+  } else {
+    const respuesta = await controller.storeFileUser(req.params.id, req.files.file);
+
+    if (respuesta) {
+      res.status(200).send(respuesta);
+    } else {
+      res.status(500).send(Response.error("Ocurrió un error inesperado"));
+    }
+  }
 });
 
 //insertar 1 o varios
-ArchivoRouter.post("/archivo",(req, res)=>{
-});
-
-//editar uno
-ArchivoRouter.put("/archivo",(req, res)=>{
-});
-
-//editar uno
-ArchivoRouter.patch("/archivo",(req, res)=>{
-});
-
-//eliminar 1
-ArchivoRouter.delete("/archivo/:id",(req, res)=>{
-});
+ArchivoRouter.post("/archivo/activo/:id", (req, res) => {});
 
 export { ArchivoRouter };
